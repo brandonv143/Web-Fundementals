f2c835d4f130fa6955539e31c5c034201cc9f518
brandonv143 <brandonvilay@gmail.com>
2021-08-22 18:08:20 -0700

    commit

--- W2D1/FizzBuss.js
+++ W2D1/FizzBuss.js
@@ -3,3 +3,13 @@
 //Multiple of 2 and 5 print "Buzz"
 //For numbers which are multiples of both 3 and 5 print "FizzBuzz"
 
+for(var i=0; i<=100; i++)
+if(i%3===0 && i%5===0){
+console.log('FizzBuzz')
+} else if(i%3===0){
+    console.log('Fizz')
+} else if(i%5 ===0){
+    console.log('Buzz')
+} else {
+    console.log(i)
+}
