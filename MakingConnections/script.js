2cf6fba8f6108285cabd1902f7e73a95f3a605ee
Brandon Vilay <brandonvilay@gmail.com>
2021-08-27 16:55:39 -0700

    commit

--- MakingConnections/script.js
+++ MakingConnections/script.js
@@ -1,7 +1,20 @@
-function turnOff(element) {
-    element.innerText = "Logout";
+var requestSpan = document.querySelector("#requests");
+var connectionSpan = document.querySelector("#connectons");
+var username = document.querySelector("#username");
+
+function accept(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestSpan.innerText--;
+    connectionSpan.innertext++;
 }
 
-function turnOff(element) {
-    element.innerText = "";
+function ignore(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestSpan.innerHTML--;
+}
+
+function edit() {
+    username.innerText = "Brandon V";
 }
